aa97cddbc7177babc5b06569ebedc8759727c630
Suman Gangopadhyay <linuxgurusuman@gmail.com>
2022-07-03 13:23:12 +0530

    Update instagram.py

--- instagram.py
+++ instagram.py
@@ -2,15 +2,16 @@ from suman import Confidential_Data
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
-
+import requests
 
 class Suman:
-    url = "https://www.instagram.com/"
+    url = "https://www.instagram.com/gangopadhyaysuman/"
+    soup = requests.get(url)
     driver = webdriver.Firefox()
 
-   def Instagram_Login(self):
-        username = "INSTAGRAM USERNAME"
-        password = "INSTAGRAM PASSWORD"
+    def Instagram_Login(self):
+        username = Confidential_Data().insta_username
+        password = Confidential_Data().insta_password
         self.driver.get(self.url)
         time.sleep(5)
         username1 = self.driver.find_element(by=By.XPATH, value='//*[@id="loginForm"]/div/div[1]/div/label/input')
@@ -23,5 +24,14 @@ class Suman:
         not_now = self.driver.find_element(by=By.XPATH, value='/html/body/div[1]/section/main/div/div/div/div/button')
         not_now.click()
 
+    def Instagram_Followers(self):
+        self.Instagram_Login()
+        time.sleep(4)
+        xpath = '/html/body/div[1]/section/main/div/header/section/ul/li[2]/a/div/span'
+        followers = self.driver.find_element(by=By.XPATH, value=xpath)
+        print(followers.text)
 
-Suman().Instagram_Login()
+s = Suman()
+
+s.Instagram_Login()
+# s.Instagram_Followers()
