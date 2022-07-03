5781d5287d7741f695a208009331bf4d4e2f0774
Suman Gangopadhyay <linuxgurusuman@gmail.com>
2022-07-03 13:08:05 +0530

    Update instagram.py

--- instagram.py
+++ instagram.py
@@ -8,22 +8,20 @@ class Suman:
     url = "https://www.instagram.com/"
     driver = webdriver.Firefox()
 
-    def Instagram_Login(self):
-        username = Confidential_Data().insta_username
-        password = Confidential_Data().insta_password
+   def Instagram_Login(self):
+        username = "INSTAGRAM USERNAME"
+        password = "INSTAGRAM PASSWORD"
         self.driver.get(self.url)
         time.sleep(5)
-        username_xpath = '//*[@id="loginForm"]/div/div[1]/div/label/input'
-        password_xpath = '//*[@id="loginForm"]/div/div[2]/div/label/input'
-        submit_button_xpath = '//*[@id="loginForm"]/div/div[3]'
-        username1 = self.driver.find_element(by=By.XPATH, value=username_xpath)
-        password1 = self.driver.find_element(by=By.XPATH, value=password_xpath)
-        submit_button = self.driver.find_element(by=By.XPATH, value=submit_button_xpath)
-
+        username1 = self.driver.find_element(by=By.XPATH, value='//*[@id="loginForm"]/div/div[1]/div/label/input')
+        password1 = self.driver.find_element(by=By.XPATH, value='//*[@id="loginForm"]/div/div[2]/div/label/input')
+        submit_button = self.driver.find_element(by=By.XPATH, value= '//*[@id="loginForm"]/div/div[3]')
         username1.send_keys(username)
-        time.sleep(5)
         password1.send_keys(password)
         submit_button.click()
+        time.sleep(5)
+        not_now = self.driver.find_element(by=By.XPATH, value='/html/body/div[1]/section/main/div/div/div/div/button')
+        not_now.click()
 
 
 Suman().Instagram_Login()
